49fec46e12e16cac57d1e7e3e5669de4e305f9ae
booldook <booldook@gmail.com>
2021-05-07 11:07:35 +0900

    bg 완료

--- js/index.js
+++ js/index.js
@@ -4,35 +4,24 @@ $(function() {
 	var time;
 	var timeDivision;
 	var weatherIcon = {
-		i01d: 'bi-brightness-high',
-		i01n: 'bi-brightness-high-fill',
-		i02d: 'bi-cloud-sun',
-		i02n: 'bi-cloud-sun-fill',
-		i03d: 'bi-cloud',
-		i03n: 'bi-cloud-fill',
-		i04d: 'bi-clouds',
-		i04n: 'bi-cloud-fills',
-		i09d: 'bi-cloud-rain-heavy',
-		i09n: 'bi-cloud-rain-heavy-fill',
-		i10d: 'bi-cloud-drizzle',
-		i10n: 'bi-cloud-drizzle-fill',
-		i11d: 'bi-cloud-lightning',
-		i11n: 'bi-cloud-lightning-fill',
-		i13d: 'bi-cloud-snow',
-		i13n: 'bi-cloud-snow-fill',
-		i50d: 'bi-cloud-haze',
-		i50n: 'bi-cloud-haze-fill',
+		i01: 'bi-brightness-high',
+		i02: 'bi-cloud-sun',
+		i03: 'bi-cloud',
+		i04: 'bi-clouds',
+		i09: 'bi-cloud-rain-heavy',
+		i10: 'bi-cloud-drizzle',
+		i11: 'bi-cloud-lightning',
+		i13: 'bi-cloud-snow',
+		i50: 'bi-cloud-haze',
 	}
 
-	var $stageWrapper = $('.stage-wrapper');
+	var $bgWrapper = $('.bg-wrapper');
 
 
 
 	/*************** 사용자 함수 *****************/
-	init();
-	function init() {
-		initBg();
-	}
+	initBg();
+
 
 	function initBg() {
 		var d = new Date();
@@ -44,8 +33,8 @@ $(function() {
 		(time >= 14 && time < 18) ? 4 :
 		(time >= 18 && time < 22) ? 5 : 6;
 
-		for(var i=1; i<=6; i++) $stageWrapper.removeClass('active'+i);
-		$stageWrapper.addClass('active'+timeDivision);
+		for(var i=1; i<=6; i++) $bgWrapper.removeClass('active'+i);
+		$bgWrapper.addClass('active'+timeDivision);
 	}
 
 
